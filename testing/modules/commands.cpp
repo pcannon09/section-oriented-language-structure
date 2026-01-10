4ea65a7ccdc7edf6971a422c7f6af58c8a56c4f4
pcannon09 <pcannon775@gmail.com>
2026-01-10 23:44:35 +0100

    Make a little template for comments

--- testing/modules/commands.cpp
+++ testing/modules/commands.cpp
@@ -1,1 +1,14 @@
+#include "commands.hpp"
+
+namespace sols::commands
+{
+	sols::ParseMessage solsComment(std::vector<std::string>)
+	{
+		sols::ParseMessage pmsg;
+
+
+
+		return pmsg;
+	}
+}
 
--- testing/modules/commands.hpp
+++ testing/modules/commands.hpp
@@ -1,6 +1,14 @@
 #ifndef INCLUDE_MODULES_COMMANDS_HPP_
 #define INCLUDE_MODULES_COMMANDS_HPP_
 
+#include "../../inc/sols/core/SOLS_parserConf.hpp"
 
+#include <string>
+#include <vector>
+
+namespace sols::commands
+{
+	sols::ParseMessage solsVersion(std::vector<std::string>);
+}
 
 #endif  // INCLUDE_MODULES_COMMANDS_HPP_
