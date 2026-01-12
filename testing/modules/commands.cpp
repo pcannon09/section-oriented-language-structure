47dbf80823a7f0e465d66339e59d29411ecf234a
pcannon09 <pcannon775@gmail.com>
2026-01-12 23:36:29 +0100

    Set comment command

--- testing/modules/commands.cpp
+++ testing/modules/commands.cpp
@@ -2,18 +2,33 @@
 
 #include "ciof/ciof.hpp"
 
+#define SOLS_COMMAND_ERRORCODE_INFOFAIL 	-2
+
 namespace sols::commands
 {
 	sols::ParseMessage solsComment(const RegisterCommand &command, const std::vector<std::string> &args)
 	{
-		sols::ParseMessage pmsg;
+    	sols::ParseMessage pmsg{};
+    	pmsg.code = 0;
 
-		pmsg.lineRange.first = command.posStart;
-		// pmsg.lineRange.second = command.file.find();
+    	// Only act on open section
+    	if (command.isOpened != SOLS_Bool::True)
+        	return pmsg;
 
-		ciof::print(command.file.substr(pmsg.lineRange.first, pmsg.lineRange.second));
+    	size_t start = command.posStart;
 
-		return pmsg;
+    	// Find closing section
+    	const std::string &toFind = ciof::format("</%1>", command.commandName);
+    	size_t end = command.file.find(toFind, start);
+
+    	if (end == std::string::npos) return pmsg;
+
+    	end += std::string(toFind).size();
+
+    	pmsg.command = SOLS_EXECCOMMAND_RETACTION_REPLACELINES;
+    	pmsg.lineRange = { start, end };
+
+    	return pmsg;
 	}
 }
 
