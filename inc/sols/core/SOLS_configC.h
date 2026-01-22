ba70385043e93ba8dad596f64ece6f772cd961ac
pcannon09 <pcannon775@gmail.com>
2026-01-22 21:34:42 +0100

    Add extern C

--- inc/sols/core/SOLS_configC.h
+++ inc/sols/core/SOLS_configC.h
@@ -1,6 +1,16 @@
 #ifndef INCLUDE_CORE_SOLS_CONFIGC_H_
 #define INCLUDE_CORE_SOLS_CONFIGC_H_
 
+#ifdef __cplusplus
+# 	define __CONFIGC_EXTERNC_OPEN 		extern "C" {
+# 	define __CONFIGC_EXTERNC_CLOSE 	}
+#else
+# 	define __CONFIGC_EXTERNC_OPEN
+# 	define __CONFIGC_EXTERNC_CLOSE
+#endif
+
+__CONFIGC_EXTERNC_OPEN
+
 #include "../SOLSpredefines.h"
 
 #define SOLS_Declare_Pair(type, type2, id) \
@@ -21,6 +31,8 @@ SOLS_Declare_Pair(int, char*, ExecCommand);
 #else
 # 	define SOLS_API
 #endif // SOLS_OS_WIN32
+
+__CONFIGC_EXTERNC_CLOSE
 
 #endif  // INCLUDE_CORE_SOLS_CONFIGC_H_
 
